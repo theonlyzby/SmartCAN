39400f22171ad7b69abc25fc8c27dd2c224992e0
Theonlyzby <theonlyzby@gmail.com>
2017-12-27 21:52:33 +0100

    Update start.sh

--- opt/start.sh
+++ opt/start.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+if [ ! -d /data/mysql ]; then
+	mkdir -p /data/mysql
+	cp -r /var/lib/mysql/* /data/mysql
+	#sed -i -e "s@^datadir.*@datadir = /data/mysql@" /etc/mysql/my.cnf
+	rm -rf /var/lib/mysql
+	chown -R mysql:mysql /data/mysql
+fi
+service mysql start
+mysql -uroot -proot -e "SHOW DATABASES";
+
 
 # Start Samba
 service samba start
@@ -13,4 +23,4 @@ service php7.0-fpm start
 /usr/local/nginx/sbin/nginx -c  /usr/local/nginx/conf/nginx.conf
 
 # To prevent Docker from exiting
-journalctl -f
+bash
