b6f6da47edd503d557a399a3ec24cfa207e93edd
Theonlyzby <theonlyzby@gmail.com>
2017-12-28 18:34:24 +0100

    Update start.sh

--- opt/start.sh
+++ opt/start.sh
@@ -10,7 +10,7 @@ chmod 0644 /etc/mysql/mariadb.conf.d/50-client.cnf
 
 service mysql start
 # Install Initial DBs
-mysql -uroot -pSmartCAN -h localhost domotique < /opt/init-DB/domotique.sql
+mysql -uroot -pSmartCAN -h localhost < /opt/init-DB/domotique.sql
 mysql -uroot -pSmartCAN -h localhost mysql < /opt/init-DB/mysql.sql
 mysql -uroot -pSmartCAN -e "SHOW DATABASES";
 
