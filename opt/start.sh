0ed75cdbb093d6742d3903768c40becdca2ea359
Theonlyzby <theonlyzby@gmail.com>
2017-12-28 18:49:27 +0100

    Update start.sh

--- opt/start.sh
+++ opt/start.sh
@@ -5,13 +5,16 @@ if [ ! -d /data/mysql ]; then
 	#sed -i -e "s@^datadir.*@datadir = /data/mysql@" /etc/mysql/my.cnf
 	rm -rf /var/lib/mysql
 	chown -R mysql:mysql /data/mysql
+	mv -r /var/www /data/www
+	service mysql start
+	# Install Initial DBs
+	mysql -uroot -pSmartCAN -h localhost < /opt/init-DB/domotique.sql
+	mysql -uroot -pSmartCAN -h localhost mysql < /opt/init-DB/mysql.sql
 fi
 chmod 0644 /etc/mysql/mariadb.conf.d/50-client.cnf
 
+# Start MySQL Server
 service mysql start
-# Install Initial DBs
-mysql -uroot -pSmartCAN -h localhost < /opt/init-DB/domotique.sql
-mysql -uroot -pSmartCAN -h localhost mysql < /opt/init-DB/mysql.sql
 mysql -uroot -pSmartCAN -e "SHOW DATABASES";
 
 # Start Samba
