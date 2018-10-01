153b5a557b53c6e755c7037f1674b44671f8fb4a
Theonlyzby <theonlyzby@gmail.com>
2018-10-01 23:04:19 +0200

    Reworked login

--- var/www/smartBACKUP/www/lang/www.login.php
+++ var/www/smartBACKUP/www/lang/www.login.php
@@ -6,12 +6,15 @@ ini_set('display_errors', '1');
 // Messages
    // EN
 $msg["login"]["Title"]["en"] = "Login";
+$msg["login"]["Error"]["en"] = "Invalid Login";
 
   // FR
 $msg["login"]["Title"]["fr"] = "LOGIN";
+$msg["login"]["Error"]["en"] = "Identifiant invalide";
 
    // NL
-$msg["STRUCTURE"]["PageTitle"]["nl"] = "";
+$msg["login"]["Title"]["nl"] = "Login";
+$msg["login"]["Error"]["en"] = "Ongeldige login";
 
 
 ?>
