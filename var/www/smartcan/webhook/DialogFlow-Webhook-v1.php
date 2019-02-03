b6007e95ff52287d88772cc357ebe0aeeed67324
Theonlyzby <theonlyzby@gmail.com>
2019-02-03 16:59:40 +0100

    DialogFlow upgrade v1 - > v2

--- var/www/smartcan/webhook/DialogFlow-Webhook-v1.php
+++ var/www/smartcan/webhook/DialogFlow-Webhook-v1.php
@@ -20,109 +20,15 @@ ob_start();
 $outputtext = "";
 $json = file_get_contents('php://input'); 
 $request = json_decode($json, true);
-$lang       = substr($request["queryResult"]["languageCode"],0,2);
-$action = $request["queryResult"]["action"];
-$userID = $request["originalDetectIntentRequest"]["payload"]["user"]["userId"];
-$parameters = $request["queryResult"]["parameters"];
+$lang       = $request["lang"];
+$action = $request["result"]["action"];
+$parameters = $request["result"]["parameters"];
 
 // Output in text file (debug)
 $file = $_SERVER['DOCUMENT_ROOT'].'/smartcan/webhook/dialowflow.txt';
 // Open the file to get existing content
-//$current = file_get_contents($file);
-$current = "Lang = ".$lang.", Action = ".$action.", UserID = ".$userID."/n<br>/n";
-
-// Action = TRAIN
-if ($action=="TRAIN") {
-	include_once('../www/lang/www.trains.php');
-	$output = "";
-	// Determine Variables Values
-	$sql = "SELECT * FROM `ha_settings` WHERE `variable`='trainShowStations';";
-	$retour = mysqli_query($DB,$sql);
-	$row = mysqli_fetch_array($retour, MYSQLI_BOTH);
-	$trainShowStations = $row["value"];
-	$sql = "SELECT * FROM `ha_settings` WHERE `variable`='trainSwitchAfterNoon';";
-	$retour = mysqli_query($DB,$sql);
-	$row = mysqli_fetch_array($retour, MYSQLI_BOTH);
-	$trainSwitchAfterNoon = $row["value"];
-
-	$url = "https://api.irail.be/connections/";
-	$sql = "SELECT * FROM `ha_settings` WHERE `variable`='trainDeparture';";
-	$retour = mysqli_query($DB,$sql);
-	$row = mysqli_fetch_array($retour, MYSQLI_BOTH);
-	if ((date("Hi")>"1200") && ($trainSwitchAfterNoon=="Y")) { $trainDestination = $row["value"]; } else { $trainDeparture = $row["value"]; }
-	$sql = "SELECT * FROM `ha_settings` WHERE `variable`='trainDestination';";
-	$retour = mysqli_query($DB,$sql);
-	$row = mysqli_fetch_array($retour, MYSQLI_BOTH);
-	// AUTO Return ?
-	if ((date("Hi")>"1200") && ($trainSwitchAfterNoon=="Y")) { $trainDeparture = $row["value"]; } else { $trainDestination = $row["value"]; }
-	// Build URL
-	$url .= "?from=" . $trainDeparture . "&to=" . $trainDestination . "&timesel=departure&format=json&lang=fr&fast=false&typeOfTransport=trains&alerts=false&results=5";
-
-	// Parses if values entered
-	if ($trainDeparture!="" && $trainDestination!="") {
-	  // Connect to SNCBNBMS API
-	  // Open the file to get existing content
-	  $curl = curl_init();
-	  curl_setopt_array($curl, array(CURLOPT_URL => $url,CURLOPT_RETURNTRANSFER => true,CURLOPT_ENCODING => "",CURLOPT_MAXREDIRS => 10,CURLOPT_TIMEOUT => 30,CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST => "GET",CURLOPT_POSTFIELDS => "",CURLOPT_HTTPHEADER => array("cache-control: no-cache"),));
-	  $dump = curl_exec($curl);
-	  $err = curl_error($curl);
-	  curl_close($curl);
-	  if ($err) {
-		$outputtext = "cURL Error #:" . $err;
-	  }
-      if (($dump!="") && ($outputtext=="")) {
-            // Parses JSON
-            $trains = json_decode($dump);
-            $count=0;
-            foreach ($trains->connection as $value) {
-              if($count==0) {
-                $count++; echo("Count" . $count . "<br>");
-                $start = $value->departure;     $departure  = date('H:i',$start->time);
-                $platform = $start->platform;
-                $outputtext .= "Le train de " . $departure . ", " . $msg["trains"]["Platform"][$lang] . " " . $platform;
-                $depDelay = $start->delay/60;
-                if ($depDelay==0) { $outputtext .= " est prÃ©vu Ã  l'heure"; } else { $outputtext .= " sera en retard de " . $depDelay . " minutes!"; }
-                $canceled = $start->canceled;
-                if ($canceled>0) { $count--; $outputtext = "Le train de " . $departure . " est annulÃ©! Le suivant est "; }
-              } // ENDIF count==0
-            } // END FOREACH
-          } // ENDIF
-	} // ENDIF
-//  $outputtext = "En retard, comme d'habitude!";
-}
-
-// 
-// Action = TEMPERATURE
-if ($action=="TEMPERATURE") {
-  /* AFFICHAGE DE LA TEMPERATURE MOYENNE DE LA MAISON */
-  $retour = mysqli_query($DB,"SELECT AVG(`valeur`) FROM `" . TABLE_CHAUFFAGE_TEMP . "` WHERE `moyenne` = '1';");
-  $row    = mysqli_fetch_array($retour, MYSQLI_BOTH);
-  $outputtext = "La tempÃ©rature ambiante est de " . round($row[0],1) . " degrÃ©s.";
-//  $outputtext = "21 degrÃ©s";
-}
-// ACTION AUGMENTER TEMPERATURE
-if ($action=="AUGMENTER") {
-  $increment = $parameters["INCREMENT"];
-  $retour = mysqli_query($DB,"SELECT `valeur` FROM `" . TABLE_CHAUFFAGE_CLEF . "` WHERE `clef` = 'temperature';");
-  $row    = mysqli_fetch_array($retour, MYSQLI_BOTH);
-  $temp    = $row[0];
-  $newTemp = $temp + $increment;
-  $retour = mysqli_query($DB,"UPDATE `" . TABLE_CHAUFFAGE_CLEF . "` SET `valeur` = " . $newTemp . " WHERE `clef` = 'temperature';");
-  $current .= "/n AUGMENTER(".$temp."->".$newTemp."), Increment=" . $increment . " sql = UPDATE `" . TABLE_CHAUFFAGE_CLEF . "` SET `valeur` = " . $newTemp . " WHERE `clef` = 'temperature';";
-  $outputtext = "La nouvelle tempÃ©rature de confort est maintenant " . $newTemp . " degrÃ©s";
-}
-// ACTION DIMINUER TEMPERATURE
-if ($action=="DIMINUER") {
-  $increment = $parameters["INCREMENT"];
-  $retour = mysqli_query($DB,"SELECT `valeur` FROM `" . TABLE_CHAUFFAGE_CLEF . "` WHERE `clef` = 'temperature';");
-  $row    = mysqli_fetch_array($retour, MYSQLI_BOTH);
-  $temp    = $row[0];
-  $newTemp = $temp - $increment;
-  $sql = "UPDATE `" . TABLE_CHAUFFAGE_CLEF . "` SET `valeur` = " . $newTemp . " WHERE `clef` = 'temperature';";
-  $retour = mysqli_query($DB,$sql);
-  $current .= "/n DIMINUER(".$temp."->".$newTemp."), Increment=" . $increment . " sql = " . $sql . "/n ";
-  $outputtext = "La nouvelle tempÃ©rature de confort est maintenant " . $newTemp . " degrÃ©s";
-}
+$current = file_get_contents($file);
+$current .= "\nLang = ".$lang.", Action = ".$action;
 
 // Action = ON
 if ($action=="ON") {
@@ -267,20 +173,20 @@ if ($action=="STATUS") {
 	$row = mysqli_fetch_array($retour, MYSQLI_BOTH);
 	//$_XTemplate->assign('TEMPERATUREEXTERIEURE', round($row[0], 1));
 	$current .=", Exterior Temperature = ".round($row[0], 1)."\n";
-	$msg .= ", \nExterior temperature: " . round($row[0], 1) . "Â°C";
+	$msg .= ", \nExterior temperature: " . round($row[0], 1) . "°C";
 
 
 	/* AFFICHAGE DE LA TEMPERATURE VOULUE */
 	$retour = mysqli_query($DB,"SELECT `valeur` FROM `" . TABLE_CHAUFFAGE_CLEF . "` WHERE `clef` = 'temperature';");
 	$row    = mysqli_fetch_array($retour, MYSQLI_BOTH);
 	//$_XTemplate->assign('TEMPERATURE', $row[0]);
-	$msg = "Set to " . $row[0] . "Â°C\n" . $msg;
+	$msg = "Set to " . $row[0] . "°C\n" . $msg;
   
 	/* AFFICHAGE DE LA TEMPERATURE MOYENNE DE LA MAISON */
 	$retour = mysqli_query($DB,"SELECT AVG(`valeur`) FROM `" . TABLE_CHAUFFAGE_TEMP . "` WHERE `moyenne` = '1';");
 	$row    = mysqli_fetch_array($retour, MYSQLI_BOTH);
 	//$_XTemplate->assign('MOYENNEMAISON', round($row[0],1));
-	$msg = "Ambiant Temperature " . round($row[0],1) . "Â°C\n" . $msg;  
+	$msg = "Ambiant Temperature " . round($row[0],1) . "°C\n" . $msg;  
   
 	// Outpur Message
 	$outputtext = $msg; //"Test Status OK!";
@@ -293,18 +199,17 @@ if ($action=="WhichPlane") {
 } // END IF
 
 // No answer
-if ($outputtext=="") {
-  $outputtext = "DÃ©solÃ©, je ne suis pas en mesure de rÃ©pondre Ã  votre quetion!"; //$outputtext = " Sorry, I cannot answer your request!";
-}
+if ($outputtext=="") { $outputtext = " Sorry, I cannot answer your request!"; }
 
 //$outputtext  = "Ok, done!";
 $nextcontext = "";
 $param1      = "";
 $param2      = "";
 
-//$output["fulfillmentMessages"] = array(array("name" => "$next-context", "parameters" =>array("param1" => $param1value, "param2" => $param2value)));
-$output["fulfillmentText"] = $outputtext;
-//$output["displayText"] = $outputtext;
+$output["contextOut"] = array(array("name" => "$next-context", "parameters" =>
+array("param1" => $param1value, "param2" => $param2value)));
+$output["speech"] = $outputtext;
+$output["displayText"] = $outputtext;
 $output["source"] = "DialogFlow-Webhook.php";
 ob_end_clean();
 echo json_encode($output);
